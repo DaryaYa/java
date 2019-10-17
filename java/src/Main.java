0ade373a2d4900b8d1e5cebbb29d7b68ff3da8a0
Darya <45762789+DaryaYa@users.noreply.github.com>
2019-10-17 15:13:56 +0300

    Add files via upload

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,12 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Кофе-машина");
-
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input money amount, please: ");
-        int moneyAmount = in.nextInt();
+        int moneyAmount = 50;
 
         int[] drinkPrices = {150, 80, 20, 50};
         String[] drinkNames = {"капучино", "эспрессо", "воду", "молоко"};
