7dcc8b0adba666d54bfce0047962bb7007c84474
Darya <45762789+DaryaYa@users.noreply.github.com>
2019-10-17 15:42:34 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+import java.awt.Robot;
+
+import static javax.imageio.ImageIO.write;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException, IOException {
         System.out.println("Кофе-машина");
 
         Scanner in = new Scanner(System.in);
@@ -23,5 +33,13 @@ public class Main {
         if(!canBuyAnything) {
             System.out.println("Not enough money");
         }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyy/MM/dd HH:mm:ss");
+        Date today = new Date();
+        System.out.println(dateFormat.format(today));
+
+        BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        ImageIO.write(image, "png", new File("d://pics/screenshot.png"));
+        System.out.println(image.getWidth() + " x " + image.getHeight());
     }
 }
